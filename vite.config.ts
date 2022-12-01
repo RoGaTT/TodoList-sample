754e14cd5f26b4bf160b274a2eef9ecadeaa78ce
RoGaTT <firestyl@yandex.ru>
2022-12-01 06:58:12 +0300

    add major features from cra

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  css: {
+    modules: {
+      generateScopedName(name, filename, css) {
+        return name;
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
+});
